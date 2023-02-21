d3f840f7dd12c39253dd9d0fcd53be6890989b70
MogulTerry13 <terrenceafanda@gmail.com>
2023-02-21 00:50:39 -0800

    printing alphabet lower ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+# include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet lower case 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char ch;
+
+		while (i < 10)
+		{
+			ch = 'a';
+			while (ch <= 'z')
+			{
+				_putchar(ch);
+				ch++;
+			}
+		_putchar('\n');
+		i++;
+		}
+}
