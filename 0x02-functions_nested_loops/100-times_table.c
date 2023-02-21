c691df129e381b4014c94c7c614e93f4058b98bd
MogulTerry13 <terrenceafanda@gmail.com>
2023-02-21 05:42:25 -0800

    times table update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,7 @@
 
 void print_times_table(int n)
 {
-	int r, c, product;
+	int r, c, product, digits;
 
 	if (n >= 0 && n <= 15)
 	{
@@ -20,25 +20,25 @@ void print_times_table(int n)
 				{
 					_putchar(product + '0');
 				}
-				else if (product < 10 && c != 0)
+				else
 				{
+					digits = 0;
+					if (product >= 10)
+						digits++;
+					if (product >= 100)
+						digits++;
 					_putchar(',');
 					_putchar(' ');
-					_putchar(product + '0');
-				}
-				else if (product >= 10 && product < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((product / 10) + '0');
-					_putchar((product % 10) + '0');
-				}
-				else if (product >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((product / 100) + '0');
-					_putchar(((product / 10) % 10) + '0');
+
+					while (digits < 3)
+					{
+						_putchar(' ');
+						digits++;
+					}
+					if (product >= 10)
+						_putchar((product / 10) + '0');
+					else
+						_putchar(' ');
 					_putchar((product % 10) + '0');
 				}
 			}
