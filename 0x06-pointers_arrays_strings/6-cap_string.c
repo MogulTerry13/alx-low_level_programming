28ee4c8934b59e3f59930480dbbe383237ce7d81
MogulTerry13 <terrenceafanda@gmail.com>
2023-03-01 11:07:47 -0800

    cap str correction

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,10 +19,9 @@ char *cap_string(char *str)
 			str[index - 1] == '\t' ||
 			str[index - 1] == '\n' ||
 			str[index - 1] == ',' ||
-			str[index - 1] == '.' ||
-			str[index - 1] == '-' ||
-			str[index - 1] == ',' ||
+			str[index - 1] == ';' ||
 			str[index - 1] == '!' ||
+			str[index - 1] == '.' ||
 			str[index - 1] == '?' ||
 			str[index - 1] == '"' ||
 			str[index - 1] == '(' ||
