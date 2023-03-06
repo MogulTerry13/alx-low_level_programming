bb011f36bb1db1605c0230efcc0264779932614e
MogulTerry13 <terrenceafanda@gmail.com>
2023-03-06 01:28:36 -0800

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (success)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				len++;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+		{
+			return (len);
+		}
+	}
+	return (len);
+}
