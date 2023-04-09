3b514cfe1272f4820396e02e2421dea755cc06ef
MogulTerry13 <terrenceafanda@gmail.com>
2023-04-09 12:13:32 -0700

    program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,63 +2,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *create_buffer(char *file);
-void close_file(int fd);
+char *allocate_buffer(char *filename);
+void close_file_descriptor(int folder);
 
 /**
- * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
+ * allocate_buffer - Allocates 1024 bytes of memory for a buffer.
+ * @filename: The name of the file whose contents will be stored in the buffer.
  *
  * Return: A pointer to the newly-allocated buffer.
  */
-char *create_buffer(char *file)
+char *allocate_buffer(char *filename)
 {
 	char *buffer;
 
 	buffer = malloc(sizeof(char) * 1024);
-
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
+		dprintf(STDERR_FILENO, "Error: Cannot write to %s\n", filename);
 		exit(99);
 	}
-
 	return (buffer);
 }
 
 /**
- * close_file - Closes file descriptors.
- * @fd: The file descriptor to be closed.
+ * close_file_descriptor - Closes a file descriptor.
+ * @folder: The file descriptor to be closed.
  */
-void close_file(int fd)
+void close_file_descriptor(int folder)
 {
-	int c;
+	int result;
 
-	c = close(fd);
+	result = close(folder);
 
-	if (c == -1)
+	if (result == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Cannot close file descriptor %d\n", folder);
 		exit(100);
 	}
 }
 
 /**
- * main - Copies the contents of a file to another file.
- * @argc: The number of arguments supplied to the program.
+ * main - Copies the contents of one file to another file.
+ * @argc: The number of arguments passed to the program.
  * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success.
  *
- * Description: If the argument count is incorrect - exit code 97.
- *              If file_from does not exist or cannot be read - exit code 98.
- *              If file_to cannot be created or written to - exit code 99.
- *              If file_to or file_from cannot be closed - exit code 100.
+ * Description: If the number of arguments is incorrect, exit with code 97.
+ *              If file does not exist or cannot be read, exit with code 98.
+ *              If the file cannot be created or written to, exit with code 99.
+ *              If either file cannot be closed, exit with code 100.
  */
 int main(int argc, char *argv[])
 {
-	int from, to, r, w;
+	int file_from, file_to, rd, wr;
 	char *buffer;
 
 	if (argc != 3)
@@ -66,39 +63,30 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	buffer = create_buffer(argv[2]);
-	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	buffer = allocate_buffer(argv[2]);
+	file_from = open(argv[1], O_RDONLY);
+	rd = read(file_from, buffer, 1024);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (from == -1 || r == -1)
+		if (file_from == -1 || rd == -1)
 		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Cannot read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
-
-		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
+		wr = write(file_to, buffer, rd);
+		if (file_to == -1 || wr == -1)
 		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Cannot write to file %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
-
-		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
-
-	} while (r > 0);
-
+		rd = read(file_from, buffer, 1024);
+		file_to = open(argv[2], O_WRONLY | O_APPEND);
+	} while (rd > 0);
 	free(buffer);
-	close_file(from);
-	close_file(to);
-
+	close_file_descriptor(file_from);
+	close_file_descriptor(file_to);
 	return (0);
 }
-
