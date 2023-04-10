177faf0059f344bdb228d55138e6a46e249956ff
MogulTerry13 <terrenceafanda@gmail.com>
2023-04-10 11:43:28 -0700

    updates

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,16 +15,17 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	op = open(filename, O_WRONLY | O_APPEND);
-	wr = write(op, text_content, length);
-	if (op == -1 || wr == -1)
-		return (-1);
-
+	/*loop to check for length*/
 	if (text_content != NULL)
 	{
 		for (length = 0; text_content[length];)
 			length++;
 	}
+	/*appending text*/
+	op = open(filename, O_WRONLY | O_APPEND);
+	wr = write(op, text_content, length);
+	if (op == -1 || wr == -1)
+		return (-1);
 	close(op);
 
 	return (1);
